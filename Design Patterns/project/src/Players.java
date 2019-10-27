937cb54beabb7b2c67b31c8b97b313df52b6044d
SaberDa <630991493@qq.com>
2019-10-26 22:49:29 -0400

    create Design Patterns basic components

---
+++ Design Patterns/project/src/Players.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Players implements Comparable<Players> {
+    private String id;
+    private String name;
+    
+}
