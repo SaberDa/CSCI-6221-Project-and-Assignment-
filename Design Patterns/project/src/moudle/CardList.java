5c60eb4c722984155cfbb745789066c7da3aefb7
SaberDa <630991493@qq.com>
2019-10-29 10:57:06 -0400

    let cardList is the child class of Card

--- Design Patterns/project/src/moudle/CardList.java
+++ Design Patterns/project/src/moudle/CardList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class CardList {
+public class CardList extends Card{
     // Store the cards
     public static List<Card> cardList = new ArrayList<Card>(52);
     // Store the result of the deal
